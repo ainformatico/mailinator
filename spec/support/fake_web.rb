7fff0491274f221a533ce0e39d17b626b397054c
Alejandro Dev <aeinformatico@gmail.com>
2014-12-28 20:42:18 +0100

    FIX fix for travis ruby 1.9.3
    Travis replaces https:// to http://:443

--- spec/support/fake_web.rb
+++ spec/support/fake_web.rb
@@ -18,3 +18,11 @@ register_uri('https://api.mailinator.com/api/email?msgid=abcd1234&token=ABCD', '
 register_uri('https://api.mailinator.com/api/email?msgid=1419696967-44152505-recipient&token=ABCD', 'email.response')
 register_uri('https://api.mailinator.com/api/not-found?token=ABCD', 'not-found.response')
 register_uri('https://api.mailinator.com/api/error?token=ABCD', 'error.response')
+
+# NOTE: fix for travis ruby 1.9.3
+#       it replaces https:// to http://:443
+register_uri('http://api.mailinator.com:443/api/inbox?to=abcd1234&token=ABCD', 'inbox.response')
+register_uri('http://api.mailinator.com:443/api/email?msgid=abcd1234&token=ABCD', 'email.response')
+register_uri('http://api.mailinator.com:443/api/email?msgid=1419696967-44152505-recipient&token=ABCD', 'email.response')
+register_uri('http://api.mailinator.com:443/api/not-found?token=ABCD', 'not-found.response')
+register_uri('http://api.mailinator.com:443/api/error?token=ABCD', 'error.response')
